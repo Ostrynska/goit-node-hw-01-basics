363f3dbf3530417081838f87221adab4106010f2
Kateryna Ostrynska <kateriinag@gmail.com>
2023-02-08 19:48:45 +0200

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,18 @@
+const contactsPath = null;
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
